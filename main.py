6e662bd9d6f7f7eb239074347d5c46520e727630
eshavyas <vyase58@gmail.com>
2023-02-05 22:17:44 -0800

    Lab 2-1

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
+# This program gets an item's original price and
+# calculates its sale price, with 20% discount.
 
-    pass
+# Get the item's orginal price.
+  original_price =float (input("Enter the item original price: "))
 
+# Calculate the amount of the discount.
+discount = "original_price" *2
 
-if __name__ == '__main__':
+# Calculate the sale price.
+sale_price = "original_price' -discount"
+
+#Display the sale price. 
+print('The sale price is', 200)
+pass
+if __name__ == '__main_':                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
     main()
